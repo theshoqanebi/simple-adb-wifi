7c24b090dff6e6e375190b4304fc1651013126fc
TheShoqanebi <contact@theshoqanebi.com>
2025-01-14 18:12:42 +0300

    App: update MainActivity view init to the new style

--- app/src/main/java/com/theshoqanebi/adbwifi/MainActivity.java
+++ app/src/main/java/com/theshoqanebi/adbwifi/MainActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.theshoqanebi.adbwifi.databinding.ActivityMainBinding;
 
@@ -22,8 +26,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         wifiStateReceiver.setOnEventListener(new OnEventListener() {
             @Override
